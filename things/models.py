f35d85bbf51e7224ab7ca07d19f0119bf7f0ff83
Sanchusri Kavitha Babu <sanchu2804kb@gmail.com>
2023-12-08 00:49:12 +0000

    Refining the Thing model

--- things/models.py
+++ things/models.py
@@ -1,7 +1,22 @@
 from django.db import models
+from django.core.validators import RegexValidator, MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(
+        max_length=30,
+        unique = True,
+        blank = False
+    )
+    description = models.CharField(
+        max_length=120, 
+        blank=True,
+        unique = False
+    )
+    quantity = models.IntegerField(
+        unique=False,
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(0)
+        ]
+    )
